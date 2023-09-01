b9d748fb9390a659951831df1dec7f3155dc9b94
laharisiri123 <sirisetti.lahari@students.iiit.ac.in>
2023-09-01 18:39:12 +0530

    update

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ console.log(curr_score);
 let high_score = 0;
 let flag = false;
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".check").addEventListener("click", function () {
   if (flag) {
     document.querySelector(".message").textContent =
@@ -16,11 +20,11 @@ document.querySelector(".check").addEventListener("click", function () {
   } else {
     let guess = Number(document.querySelector(".guess").value);
     if (!guess) {
-      document.querySelector(".message").textContent = "No number entered!";
+      displayMessage("No number entered!");
     } else {
       // equal to secret NUmber
       if (random_number === guess) {
-        document.querySelector(".message").textContent = "Correct Guess!!!";
+        displayMessage("Correct Guess!!!");
         high_score = Math.max(high_score, curr_score);
         document.querySelector("#highScoreValue").textContent = high_score;
         document.querySelector(".number").textContent = random_number;
@@ -28,28 +32,14 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector("body").style.backgroundColor = "white";
         document.querySelector(".message").style.color = "blue";
 
-        // lower than secret Number
-      } else if (random_number > guess) {
-        if (curr_score > 1) {
-          document.querySelector(".message").textContent = "Too low";
-          curr_score--;
-          document.querySelector("#scoreValue").textContent = curr_score; // Update displayed score
-        } else {
-          document.querySelector(".message").textContent =
-            "You lost the game :(";
-          curr_score = 0;
-          document.querySelector("#scoreValue").textContent = curr_score; // Update displayed score
-        }
-
-        // Higher than secret number
+        // not equal to secret Number
       } else {
         if (curr_score > 1) {
-          document.querySelector(".message").textContent = "Too high";
+          displayMessage(random_number > guess ? "Too low" : "Too high");
           curr_score--;
           document.querySelector("#scoreValue").textContent = curr_score; // Update displayed score
         } else {
-          document.querySelector(".message").textContent =
-            "You lost the game :(";
+          displayMessage("You lost the game :(");
           curr_score = 0;
           document.querySelector("#scoreValue").textContent = curr_score; // Update displayed score
         }
@@ -63,7 +53,7 @@ document.querySelector(".restart").addEventListener("click", function () {
   curr_score = 20;
   document.querySelector("#scoreValue").textContent = 20;
   document.querySelector(".number").textContent = "?";
-  document.querySelector(".message").textContent = "Start Guessing!";
+  displayMessage("Start Guessing!");
   // Reset the input box with placeholder
   let guessInput = document.querySelector(".guess");
   guessInput.value = ""; // Clear any previous input
